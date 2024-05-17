77a69c87c5a39920d2898d5476cd89266dd86404
tleavitt25526 <98976857+tleavitt25526@users.noreply.github.com>
2024-05-17 15:47:07 -0400

    guh

---
+++ images.g.ts
@@ -0,0 +1,40 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "l#0/+~;e":
+            case "greenCell":return img`
+7 
+`;
+            case "7S0h3mO~lJ`e_$wY{?gP":
+            case "redCell":return img`
+2 2 
+2 2 
+`;
+            case "image1":
+            case "purpleCell":return img`
+a a a 
+a a a 
+a a a 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,1 +1,53 @@
-
+namespace SpriteKind {
+    export const Green = SpriteKind.create()
+    export const Red = SpriteKind.create()
+    export const Purple = SpriteKind.create()
+}
+function spawnPurple () {
+    purpleCell = sprites.create(assets.image`purpleCell`, SpriteKind.Purple)
+    purpleCell.setPosition(randint(0, 160), randint(0, 120))
+    purpleCell.setStayInScreen(true)
+    purpleCell.setBounceOnWall(true)
+}
+spriteutils.onSpriteKindUpdateInterval(SpriteKind.Purple, 1000, function (sprite) {
+    spriteutils.setVelocityAtAngle(sprite, randint(0, 359), 5)
+})
+function spawnRed () {
+    redCell = sprites.create(assets.image`redCell`, SpriteKind.Red)
+    redCell.setPosition(randint(0, 160), randint(0, 120))
+    redCell.setStayInScreen(true)
+    redCell.setBounceOnWall(true)
+}
+sprites.onOverlap(SpriteKind.Red, SpriteKind.Green, function (sprite, otherSprite) {
+    otherSprite.setImage(assets.image`redCell`)
+    otherSprite.setKind(SpriteKind.Red)
+})
+spriteutils.onSpriteKindUpdateInterval(SpriteKind.Red, 1000, function (sprite) {
+    spriteutils.setVelocityAtAngle(sprite, randint(0, 359), 20)
+    if (spriteutils.getSpritesWithin(SpriteKind.Green, 50, sprite).length > 0) {
+        select = spriteutils.getSpritesWithin(SpriteKind.Green, 50, sprite)._pickRandom()
+        spriteutils.setVelocityAtAngle(sprite, spriteutils.angleFrom(sprite, select), 30)
+    }
+})
+spriteutils.onSpriteKindUpdateInterval(SpriteKind.Green, 1000, function (sprite) {
+    spriteutils.setVelocityAtAngle(sprite, randint(0, 359), 10)
+    if (spriteutils.getSpritesWithin(SpriteKind.Red, 50, sprite).length > 0) {
+        select = spriteutils.getSpritesWithin(SpriteKind.Red, 50, sprite)._pickRandom()
+        spriteutils.setVelocityAtAngle(sprite, 180 + spriteutils.angleFrom(sprite, select), 15)
+    }
+})
+function spawnGreen () {
+    greenCell = sprites.create(assets.image`greenCell`, SpriteKind.Green)
+    greenCell.setPosition(randint(0, 160), randint(0, 120))
+    greenCell.setStayInScreen(true)
+    greenCell.setBounceOnWall(true)
+}
+let greenCell: Sprite = null
+let select: Sprite = null
+let redCell: Sprite = null
+let purpleCell: Sprite = null
+for (let index = 0; index < 50; index++) {
+    spawnGreen()
+}
+spawnRed()
+spawnPurple()
